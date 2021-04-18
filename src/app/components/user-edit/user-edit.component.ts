aedd988b37615190a140308dcf972313702b8bfb
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-04-18 17:07:46 -0500

    Sección 21: Ajustes de usuario (front-end con Angular) -> 72. Rellenar el fomulario de ajustes
    Rellenar el formulario de Ajustes con los datos del usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
   public page_title: string;
   public user: User;
+  public identity;
+  public token;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = "Ajustes de usuario";
     this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '', '');
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+
+    //RELLENAR OBJETO USUARIO
+    this.user = this.identity.user;
+    console.log(this.identity);
   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form){
+
+  }
 }
