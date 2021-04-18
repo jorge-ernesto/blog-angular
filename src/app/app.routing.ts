2a090b64cf56cc60a3bccdfc0f9749473e061888
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-04-18 14:12:01 -0500

    Sección 18: Routing y menús -> 58. Creacion de componentes del proyecto
    Creamos component home y error
    ng g component components/home
    ng g component components/error
    
    Se agrego la variable page_title
    Se agrearon componentes al fichero de las rutas (app.routing.ts)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,20 @@
 //IMPORTS NECESARIOS
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 
 //IMPORTAR COMPONENTES
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HomeComponent} from './components/home/home.component';
+import {ErrorComponent} from './components/error/error.component';
 
 //DEFINIR RUTAS
 const APP_ROUTES: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'inicio', component: LoginComponent},
+    {path: 'inicio', component: LoginComponent},    
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: ErrorComponent}
 ];
 
 //EXPORTAR CONFIGURACION
