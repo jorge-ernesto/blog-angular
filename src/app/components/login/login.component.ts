43d72edd8b07819228c0f3384b47e5d2e9486d33
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-04-18 16:14:10 -0500

    Sección 20: Login de usuarios (webapp con Angular) -> 65. Formulario de login
    Creamos formulario login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public page_title: string; //Creamos una propiedad publica
+  public user: User;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Identificate'; //Lo normal es darle un valor a las propiedades dentro del constructor
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    console.log(this.user);
   }
 
 }
