a12405a67152428da5f09efb5be9562ffbd572a5
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-04-18 14:49:51 -0500

    Sección 19: Registro de usuarios (web SPA con Angular) -> 61. Formulario de registro con Angular
    Creamos la function onSubmit, los ngModel y validaciones para que sea un formulario de Angular

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
+  public user: User;
   
   constructor() { 
     this.page_title = "Registrate";
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+
+    /*
+    public id: number,
+    public name: string,
+    public surname: string,
+    public role: string,
+    public email: string,
+    public password: string,
+    public description: string,
+    public image:string
+    */
   }
 
   ngOnInit(): void {
     console.log("Componente de registro lanzado");
+  }
+
+  onSubmit(form){
+    console.log(this.user);
+    console.log(form);
   }
 
 }
