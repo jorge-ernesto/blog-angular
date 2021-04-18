3375d106a365f60ec22958a44c7e4bace7633f98
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-04-18 17:43:56 -0500

    Sección 22: Categorías en el frontend -> 80. Vista de creación de categorias
    Vista de creación de categorias

--- src/app/components/category-new/category-new.component.ts
+++ src/app/components/category-new/category-new.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {UserService} from './../../services/user.service';
+import {Category} from './../../models/category'; 
 
 @Component({
   selector: 'app-category-new',
   templateUrl: './category-new.component.html',
-  styleUrls: ['./category-new.component.css']
+  styleUrls: ['./category-new.component.css'],
+  providers: [UserService]
 })
 export class CategoryNewComponent implements OnInit {
+  public page_title: string;
+  public status: string;
+  public identity;
+  public token;
+  public category: Category;  
 
-  constructor() { }
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    this.page_title = "Crear nueva categoria";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.category = new Category(1, '');
+  }
 
   ngOnInit(): void {
   }
